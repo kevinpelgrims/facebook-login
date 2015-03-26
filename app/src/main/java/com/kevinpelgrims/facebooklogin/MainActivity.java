6f3f8f9b9cfd82bf3afbe656de412f47deb7dc0d
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2015-03-26 10:40:34 +0100

    Initial commit
    Includes the Facebook SDK and a basic login implementation, displaying the user's name and email.

---
+++ app/src/main/java/com/kevinpelgrims/facebooklogin/MainActivity.java
@@ -0,0 +1,92 @@
+package com.kevinpelgrims.facebooklogin;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+public class MainActivity extends ActionBarActivity {
+    private UiLifecycleHelper uiHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login);
+        loginButton.setReadPermissions("public_profile", "email");
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Session session = Session.getActiveSession();
+                if (!session.isOpened() && !session.isClosed()) {
+                    session.openForRead(new Session.OpenRequest(MainActivity.this)
+                            .setPermissions("public_profile", "email")
+                            .setCallback(statusCallback));
+                } else {
+                    Session.openActiveSession(MainActivity.this, true, statusCallback);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(MainActivity.this, requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception e) {
+            if (state.isOpened()) {
+                ((TextView) findViewById(R.id.token)).setText(session.getAccessToken());
+
+                Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+                    @Override
+                    public void onCompleted(GraphUser user, Response response) {
+                        if (user != null) {
+                            String userString = String.format("%s %s (%s)", user.getFirstName(),
+                                                                            user.getLastName(),
+                                                                            user.getProperty("email") != null ? user.getProperty("email").toString() : "no email");
+                            ((TextView) findViewById(R.id.email)).setText(userString);
+                        }
+                    }
+                });
+                request.executeAsync();
+            }
+        }
+    };
+}
